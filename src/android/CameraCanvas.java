5df938240bfb4cf934b4af03f16da073e7d2630b
arooj fatima <arooj.fatima@gmail.com>
2015-04-09 02:57:39 +0100

    tt

--- src/android/CameraCanvas.java
+++ src/android/CameraCanvas.java
@@ -8,6 +8,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CameraCanvas extends CordovaPlugin{
+	 private int _quality = 85;
+	    private int _destType = DestinationTypeDataURL;
+	    private boolean _allowEdit = false;
+	    private int _encodeType = EncodingTypeJPEG;
+	    private boolean _saveToPhotoAlbum = false;
+	    private boolean _correctOrientation = true;
+
+	    private int _width = 640;
+	    private int _height = 480;
+
 	@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("show")) {
@@ -24,5 +34,32 @@ public class CameraCanvas extends CordovaPlugin{
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+    private void start(JSONArray args, CallbackContext callbackContext) {
+    	   _quality = 85;
+           _destType = DestinationTypeFileURI;
+           _encodeType = EncodingTypeJPEG;
+           _saveToPhotoAlbum = false;
+           _correctOrientation = true;
+           _width = 640;
+           _height = 480;
+
+           // parse options
+           if (args.length() > 0)
+           {
+               try {
+                   JSONObject jsonData = args.getJSONObject(0);
+                   getOptions(jsonData);
+               } catch (Exception e) {
+                   Log.d("CanvasCamera", "Parsing options error : " + e.getMessage());
+               }
+           }
+        if (args != null && args.length() > 0) {
+            callbackContext.success(args);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+        Intent intent = new Intent(this.cordova.getActivity(), CameraCanvasView.class);
+		this.cordova.startActivityForResult(this, intent, CANVAS_CAMERA);
     }
 }
